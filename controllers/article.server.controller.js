059544ac6a6bd0ca061947e55c08e90feeb3487b
Tabo Mbaleni <tmbaleni@live.com>
2019-02-28 18:21:53 +0800

    added comments to article.server.controller

--- controllers/article.server.controller.js
+++ controllers/article.server.controller.js
@@ -4,26 +4,35 @@ import mongoose from 'mongoose';
 //import models
 import Articles from '../model/article.server.model';
 
+//get articles all from '/api/'
 export const getArticles = (req,res) => {
+    //find all articles in database
     Articles.find().exec((err,articles) => {
       if(err){
+      //return error or    
       return res.json({'success':false,'message':'Some Error'});
       }
+      //return succes and articles
   return res.json({'success':true,'message':'Articles fetched successfully',articles});
     });
   }
-
+//post article all from '/api/'
 export const addArticle = (req,res) => {
+    //extract params from body into schema
     const newArticle = new Articles(req.body);
+    //add to database
     newArticle.save((err,article) => {
       if(err){
+        //retun error
       return res.json({'success':false,'message':'Some Error'});
       }
+       //return succes
   return res.json({'success':true,'message':'Article added successfully',article});
     })
 }
-
+//PUT: update an article by id /api/
 export const updateArticle = (req,res) => {
+    //find and update from body params
     Articles.findOneAndUpdate({ _id:req.body.id }, req.body, { new:true }, (err,article) => {
       if(err){
       return res.json({'success':false,'message':'Some Error','error':err});
@@ -32,12 +41,13 @@ export const updateArticle = (req,res) => {
       return res.json({'success':true,'message':'Updated successfully',article});
     })
 }
-
+//GET find article by id /api/8099
 export const getArticle = (req,res) => {
     Articles.find({_id:req.params.id}).exec((err,article) => {
       if(err){
       return res.json({'success':false,'message':'Some Error'});
       }
+      //if article length is greater than 0 return article
       if(article.length){
         return res.json({'success':true,'message':'Article fetched by id successfully',article});
       }
@@ -46,7 +56,7 @@ export const getArticle = (req,res) => {
       }
     })
 }
-
+//DELETE delete an article by id /api/89099
 export const deleteArticle = (req,res) => {
     Articles.findByIdAndRemove(req.params.id, (err,article) => {
       if(err){
